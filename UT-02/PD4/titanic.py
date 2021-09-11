473c0f4eca711b851105246288080d0d3b3c01ee
Felipe Mestre <42158804+FelipeMestre@users.noreply.github.com>
2021-09-11 01:09:55 -0300

    UT2 PD4

---
+++ UT-02/PD4/titanic.py
@@ -0,0 +1,121 @@
+import numpy as np 
+import pandas as pd 
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+import seaborn as sns
+from matplotlib import pyplot as plt
+sns.set_style("whitegrid")
+#%matplotlib inline
+
+import warnings
+warnings.filterwarnings("ignore")
+
+training = pd.read_csv("train.csv")
+testing = pd.read_csv("test.csv")
+
+training.head()
+testing.head()
+
+def null_table(training, testing):
+    print("---Training---")
+    print(pd.isnull(training).sum()) 
+    print(" ")
+    print("---Testing---")
+    print(pd.isnull(testing).sum())
+
+null_table(training, testing)
+
+copy = training.copy()
+copy.dropna(inplace = True)
+sns.distplot(copy["Age"])
+
+training.drop(labels = ["Cabin", "Ticket"], axis = 1, inplace = True)
+testing.drop(labels = ["Cabin", "Ticket"], axis = 1, inplace = True)
+
+training["Age"].fillna(training["Age"].median(), inplace = True)
+testing["Age"].fillna(testing["Age"].median(), inplace = True) 
+training["Embarked"].fillna("S", inplace = True)
+testing["Fare"].fillna(testing["Fare"].median(), inplace = True)
+
+null_table(training, testing)
+
+sns.barplot(x="Sex", y="Survived", data=training)
+plt.title("Distribution of Survival based on Gender")
+plt.show()
+
+le_sex = LabelEncoder()
+le_sex.fit(training["Sex"])
+
+encoded_sex_training = le_sex.transform(training["Sex"])
+training["Sex"] = encoded_sex_training
+encoded_sex_testing = le_sex.transform(testing["Sex"])
+testing["Sex"] = encoded_sex_testing
+
+le_embarked = LabelEncoder()
+le_embarked.fit(training["Embarked"])
+
+encoded_embarked_training = le_embarked.transform(training["Embarked"])
+training["Embarked"] = encoded_embarked_training
+encoded_embarked_testing = le_embarked.transform(testing["Embarked"])
+testing["Embarked"] = encoded_embarked_testing
+
+training.head()
+
+for name in training["Name"]:
+    training["Title"] = training["Name"].str.extract("([A-Za-z]+)\.",expand=True)
+    
+for name in testing["Name"]:
+    testing["Title"] = testing["Name"].str.extract("([A-Za-z]+)\.",expand=True)
+
+titles = set(training["Title"])
+title_list = list(training["Title"])
+frequency_titles = []
+
+for i in titles:
+    frequency_titles.append(title_list.count(i))
+
+titles = list(titles)
+
+title_dataframe = pd.DataFrame({
+    "Titles" : titles,
+    "Frequency" : frequency_titles
+})
+
+print(title_dataframe)
+
+title_replacements = {"Mlle": "Other", "Major": "Other", "Col": "Other", "Sir": "Other", "Don": "Other", "Mme": "Other",
+          "Jonkheer": "Other", "Lady": "Other", "Capt": "Other", "Countess": "Other", "Ms": "Other", "Dona": "Other"}
+
+training.replace({"Title": title_replacements}, inplace=True)
+testing.replace({"Title": title_replacements}, inplace=True)
+
+le_title = LabelEncoder()
+le_title.fit(training["Title"])
+
+encoded_title_training = le_title.transform(training["Title"])
+training["Title"] = encoded_title_training
+encoded_title_testing = le_title.transform(testing["Title"])
+testing["Title"] = encoded_title_testing
+
+training.drop("Name", axis = 1, inplace = True)
+testing.drop("Name", axis = 1, inplace = True)
+
+training.head()
+
+training["FamSize"] = training["SibSp"] + training["Parch"] + 1
+testing["FamSize"] = testing["SibSp"] + testing["Parch"] + 1
+
+training.head()
+
+scaler = StandardScaler()
+
+ages_train = np.array(training["Age"]).reshape(-1, 1)
+fares_train = np.array(training["Fare"]).reshape(-1, 1)
+ages_test = np.array(testing["Age"]).reshape(-1, 1)
+fares_test = np.array(testing["Fare"]).reshape(-1, 1)
+
+training["Age"] = scaler.fit_transform(ages_train)
+training["Fare"] = scaler.fit_transform(fares_train)
+testing["Age"] = scaler.fit_transform(ages_test)
+testing["Fare"] = scaler.fit_transform(fares_test)
+
+training.head()
