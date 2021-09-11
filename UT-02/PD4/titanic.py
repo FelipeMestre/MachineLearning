314868b651267dd4fdeca88f2d58c4645e9b8dc3
Felipe Mestre <42158804+FelipeMestre@users.noreply.github.com>
2021-09-11 19:11:28 -0300

    UT02 PD4

--- UT-02/PD4/titanic.py
+++ UT-02/PD4/titanic.py
@@ -2,33 +2,52 @@ import numpy as np
 import pandas as pd 
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 import seaborn as sns
-from matplotlib import pyplot as plt
 sns.set_style("whitegrid")
-#%matplotlib inline
-
+from matplotlib import pyplot as plt
+from IPython.display import display
 import warnings
 warnings.filterwarnings("ignore")
+from sklearn.linear_model import LogisticRegression
 
 training = pd.read_csv("train.csv")
 testing = pd.read_csv("test.csv")
 
-training.head()
-testing.head()
+
+print("=================== FIRST ROWS TRAINING ===================")
+display(training.head())
+print("=================== FIRST ROWS TEST ===================")
+display(testing.head())
+
+print("=================== TRAINIGN KEYS ===================")
+display(training.keys())
+print("=================== TEST KEYS ===================")
+display(testing.keys())
+
+types_train = training.dtypes
+num_values = types_train[(types_train == float)]
+
+print("=================== These are the numerical features: ===================")
+print(num_values)
+
+training.describe()
 
 def null_table(training, testing):
-    print("---Training Nulls---")
+    print("=================== Training Nulls ===================")
     print(pd.isnull(training).sum()) 
     print(" ")
-    print("---Testing Nulls---")
+    print("=================== Testing Nulls ===================")
     print(pd.isnull(testing).sum())
 
 null_table(training, testing)
 
+print("=================== Distribution of age ===================")
 copy = training.copy()
 copy.dropna(inplace = True)
+plot1 = plt.figure(1)
 sns.distplot(copy["Age"])
 
-#Substitute Null values
+
+# #Substitute Null values
 training.drop(labels = ["Cabin", "Ticket"], axis = 1, inplace = True)
 testing.drop(labels = ["Cabin", "Ticket"], axis = 1, inplace = True)
 
@@ -39,20 +58,86 @@ testing["Fare"].fillna(testing["Fare"].median(), inplace = True)
 
 null_table(training, testing)
 
-print("There are no nulls !!")
+training.head()
+testing.head()
 
+print("=================== There are no nulls !! ===================")
+plot2 = plt.figure(2)
 sns.barplot(x="Sex", y="Survived", data=training)
 plt.title("Distribution of Survival based on Gender")
-plt.show()
 
+plot3 = plt.figure(3)
 sns.stripplot(x="Survived", y="Age", data=training, jitter=True)
-
 sns.barplot(x="Pclass", y="Survived", data=training)
 plt.ylabel("Survival Rate")
 plt.title("Distribution of Survival Based on Class")
-plt.show()
 
-#Lets make all atributes numerical
+
+total_survived_females = training[training.Sex == "female"]["Survived"].sum()
+total_survived_males = training[training.Sex == "male"]["Survived"].sum()
+
+print("=================== PROPORTION OF SURVIVORS BASED ON GENDER ===================")
+print("Total people survived is: " + str((total_survived_females + total_survived_males)))
+print("Proportion of Females who survived:") 
+print(total_survived_females/(total_survived_females + total_survived_males))
+print("Proportion of Males who survived:")
+print(total_survived_males/(total_survived_females + total_survived_males))
+
+plot4 = plt.figure(4)
+sns.barplot(x="Pclass", y="Survived", data=training)
+plt.ylabel("Survival Rate")
+plt.title("Distribution of Survival Based on Class")
+
+total_survived_one = training[training.Pclass == 1]["Survived"].sum()
+total_survived_two = training[training.Pclass == 2]["Survived"].sum()
+total_survived_three = training[training.Pclass == 3]["Survived"].sum()
+total_survived_class = total_survived_one + total_survived_two + total_survived_three
+
+print("=================== PROPORTION OF SURVIVOR BASED ON PASSANGER CLASS ===================")
+print("Total people survived is: " + str(total_survived_class))
+print("Proportion of Class 1 Passengers who survived:") 
+print(total_survived_one/total_survived_class)
+print("Proportion of Class 2 Passengers who survived:")
+print(total_survived_two/total_survived_class)
+print("Proportion of Class 3 Passengers who survived:")
+print(total_survived_three/total_survived_class)
+
+print("=================== PROPORTION OF SURVIVOR BASED ON GENDER AND CLASS ===================")
+plot5 = plt.figure(5)
+sns.barplot(x="Pclass", y="Survived", hue="Sex", data=training)
+plt.ylabel("Survival Rate")
+plt.title("Survival Rates Based on Gender and Class")
+
+plot6 = plt.figure(6)
+sns.barplot(x="Sex", y="Survived", hue="Pclass", data=training)
+plt.ylabel("Survival Rate")
+plt.title("Survival Rates Based on Gender and Class")
+
+print("=================== PROPORTION OF SURVIVOR BASED ON AGE ===================")
+survived_ages = training[training.Survived == 1]["Age"]
+not_survived_ages = training[training.Survived == 0]["Age"]
+plot7 = plt.figure(7)
+plt.subplot(1, 2, 1)
+sns.distplot(survived_ages, kde=False)
+plt.axis([0, 100, 0, 100])
+plt.title("Survived")
+plt.ylabel("Proportion")
+plt.subplot(1, 2, 2)
+sns.distplot(not_survived_ages, kde=False)
+plt.axis([0, 100, 0, 100])
+plt.title("Didn't Survive")
+
+print("=================== ALL FEATURES GRAPHICS ===================")
+plot8 = plt.figure(8)
+sns.stripplot(x="Survived", y="Age", data=training, jitter=True)
+
+sns.pairplot(training)
+#plt.show()
+
+
+print ("=================== FEATURE ENGINEERING ===================")
+# #Lets make all atributes numerical
+
 le_sex = LabelEncoder()
 le_sex.fit(training["Sex"])
 
@@ -69,7 +154,10 @@ training["Embarked"] = encoded_embarked_training
 encoded_embarked_testing = le_embarked.transform(testing["Embarked"])
 testing["Embarked"] = encoded_embarked_testing
 
-training.head()
+training["FamSize"] = training["SibSp"] + training["Parch"] + 1
+testing["FamSize"] = testing["SibSp"] + testing["Parch"] + 1
+training["IsAlone"] = training.FamSize.apply(lambda x: 1 if x == 1 else 0)
+testing["IsAlone"] = testing.FamSize.apply(lambda x: 1 if x == 1 else 0)
 
 for name in training["Name"]:
     training["Title"] = training["Name"].str.extract("([A-Za-z]+)\.",expand=True)
@@ -77,12 +165,18 @@ for name in training["Name"]:
 for name in testing["Name"]:
     testing["Title"] = testing["Name"].str.extract("([A-Za-z]+)\.",expand=True)
 
-titles = set(training["Title"])
+display(training.head())
+#CHECKEAR ESTO
+titles = set(training["Title"]) #making it a set gets rid of all duplicates
+print(titles)
+
 title_list = list(training["Title"])
 frequency_titles = []
 
 for i in titles:
     frequency_titles.append(title_list.count(i))
+    
+print(frequency_titles)
 
 titles = list(titles)
 
@@ -110,23 +204,40 @@ testing["Title"] = encoded_title_testing
 training.drop("Name", axis = 1, inplace = True)
 testing.drop("Name", axis = 1, inplace = True)
 
-training.head()
+display(training.sample(5))
+print("======================= ALL FEATURES ARE NUMERICAL FAMILY ONES ARE COMBINED AND NAME WAS USED TO MAKE THE TITLE COLUMN =======================")
+print("======================= LET'S MAKE LOGISTIC REGRESSION =======================")
 
-training["FamSize"] = training["SibSp"] + training["Parch"] + 1
-testing["FamSize"] = testing["SibSp"] + testing["Parch"] + 1
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import make_scorer, accuracy_score 
+from sklearn.model_selection import GridSearchCV
+from sklearn.model_selection import train_test_split #to create validation data set
 
-training.head()
+X_train = training.drop(labels=["PassengerId", "Survived"], axis=1) #define training features set
+y_train = training["Survived"] #define training label set
+X_test = testing.drop("PassengerId", axis=1) #define testing features set
+#we don't have y_test, that is what we're trying to predict with our model
+display(X_train.head())
 
-scaler = StandardScaler()
 
-ages_train = np.array(training["Age"]).reshape(-1, 1)
-fares_train = np.array(training["Fare"]).reshape(-1, 1)
-ages_test = np.array(testing["Age"]).reshape(-1, 1)
-fares_test = np.array(testing["Fare"]).reshape(-1, 1)
+X_training, X_valid, y_training, y_valid = train_test_split(X_train, y_train, test_size=0.2, random_state=0) #X_valid and y_valid are the validation sets
 
-training["Age"] = scaler.fit_transform(ages_train)
-training["Fare"] = scaler.fit_transform(fares_train)
-testing["Age"] = scaler.fit_transform(ages_test)
-testing["Fare"] = scaler.fit_transform(fares_test)
+dt_clf = DecisionTreeClassifier()
 
-training.head()
+parameters_dt = {"criterion": ["gini", "entropy"], "splitter": ["best", "random"], "max_features": ["auto", "sqrt", "log2"]}
+
+grid_dt = GridSearchCV(dt_clf, parameters_dt, scoring=make_scorer(accuracy_score))
+grid_dt.fit(X_training, y_training)
+
+dt_clf = grid_dt.best_estimator_
+
+dt_clf.fit(X_training, y_training)
+pred_dt = dt_clf.predict(X_valid)
+acc_dt = accuracy_score(y_valid, pred_dt)
+
+print("The Score for Decision Tree is: " + str(acc_dt))
